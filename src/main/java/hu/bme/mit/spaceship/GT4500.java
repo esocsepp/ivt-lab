0d975ccb9d0225162c24fcaa14f8dcf770e38ce3
esocsepp <47364171+esocsepp@users.noreply.github.com>
2021-04-18 16:04:23 +0200

    case ALL implemented

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,13 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        if (! secondaryTorpedoStore.isEmpty() && ! primaryTorpedoStore.isEmpty()) {
+          firingSuccess = secondaryTorpedoStore.fire(1);
+          firingSuccess = primaryTorpedoStore.fire(1);
+        }
+        else {
+          // if any of the stores are empty, nothing can be done, return failure
+        }
 
         break;
     }
